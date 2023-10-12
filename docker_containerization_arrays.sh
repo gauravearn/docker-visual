fd04c7fff8d6ea8e6b8a4afb0f0bff3fdf9665bb
Gaurav Sablok <sablokg@gmail.com>
2023-10-12 21:31:44 +0530

    Update docker_containerization_arrays.sh

--- docker_containerization_arrays.sh
+++ docker_containerization_arrays.sh
@@ -3,6 +3,7 @@
 # Author: Gaurav Sablok
 # date: 2023-10-12
 # MIT License
-$ docker image ls >> docker_images.txt 
-open docker_images.txt | lines | split column "  " | get column1 | str trim | sort | each { |each| $"Docker repository running, ($each)!" } 
-open docker_images.txt | lines | split column "  " | get column1 | str trim | sort | each { |each| ($docker_image | prepend ($each)) }
+$ docker image ls >> docker_images.txt
+$let docker_image = []
+$open docker_images.txt | lines | split column "  " | get column1 | str trim | sort | each { |each| $"Docker repository running, ($each)!" } 
+$open docker_images.txt | lines | split column "  " | get column1 | str trim | sort | each { |each| ($docker_image | prepend ($each)) }
