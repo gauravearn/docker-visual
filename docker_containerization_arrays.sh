650dd7bc50a859bb5c070fd503fe6842a40067ea
Gaurav Sablok <sablokg@gmail.com>
2023-10-12 21:22:48 +0530

    Create docker_containerization_arrays.sh

---
+++ docker_containerization_arrays.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# -*- coding:  utf-8 -*-
+# Author: Gaurav Sablok
+# date: 2023-10-12
+# MIT License
+$ docker image ls >> docker_images.txt 
+open docker_images.txt | lines | split column "  " | get column1 | str trim | sort | each { |each| $"Docker repository running, ($each)!" } 
+open docker_images.txt | lines | split column "  " | get column1 | str trim | sort | each { |each| ($docker_image | prepend ($each)) }
